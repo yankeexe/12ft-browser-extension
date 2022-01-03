6a3d3ac3f80e81e54c36b385836f97b2b63dc840
Yankee Maharjan <yankee.exe@gmail.com>
2022-01-03 11:15:05 +0545

    Update script

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-IGNORE_PAGES = []
 chrome.browserAction.onClicked.addListener(() => {
 	chrome.tabs.query(
 		{ currentWindow: true, active: true },
 		function (tab) {
 			let activeTab = new URL(tab[0].url);
-			if (activeTab.protocol != "https:") { return; }
-
-			chrome.tabs.update({
-				url: `https://12ft.io/${activeTab.href}`,
-			});
+			if (activeTab.protocol == "https:") {
+				chrome.tabs.update({
+					url: `https://12ft.io/${activeTab.href}`,
+				});
+			}
 		}
 	);
 });
